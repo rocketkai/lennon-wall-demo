7b81fe1e4f0673fe832269a18774ecdb0510241a
Kai Yuan Neo <kai@rocketacademy.co>
2020-02-25 00:27:36 +0800

    completed redux react router tutorial

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from "./components/App";
+import configureStore from "./configureStore";
 
-import App from './components/App';
-import configureStore from './configureStore';
-
-const store = configureStore()
+const store = configureStore();
 
 render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/:filter?" component={App} />
+    </Router>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
