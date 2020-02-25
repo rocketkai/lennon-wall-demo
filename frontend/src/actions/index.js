54b9f979d4f1b56a23595a0eb9f926b150421713
Kai Yuan Neo <kai@rocketacademy.co>
2020-02-25 11:42:24 +0800

    fixed formatting with prettier

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,45 +1,44 @@
-
-let nextTodoId = 0
+let nextTodoId = 0;
 export const addTodo = value => ({
-  type: 'ADD_TODO',
+  type: "ADD_TODO",
   id: nextTodoId++,
   value
-})
+});
 
 export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
+  type: "SET_VISIBILITY_FILTER",
   filter
-})
+});
 
 export const requestTodos = () => ({
-  type: 'REQUEST_TODOS'
-})
+  type: "REQUEST_TODOS"
+});
 
 export const receiveTodos = todos => ({
-  type: 'RECEIVE_TODOS',
-  todos,
-})
+  type: "RECEIVE_TODOS",
+  todos
+});
 
 export const fetchTodos = () => dispatch => {
   dispatch(requestTodos());
-  return fetch('http://localhost:4000/todos/retrieve')
+  return fetch("http://localhost:4000/todos/retrieve")
     .then(response => {
       console.log(response);
-      return response.json()
+      return response.json();
     })
     .then(todos => {
       console.log(todos);
-      return dispatch(receiveTodos(todos))
-    })
-}
+      return dispatch(receiveTodos(todos));
+    });
+};
 
 export const toggleTodo = id => ({
-  type: 'TOGGLE_TODO',
+  type: "TOGGLE_TODO",
   id
-})
+});
 
 export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+};
